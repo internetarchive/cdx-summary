28aebf4473d606a6d33a26ca4b87bd9fa3df669e
Sawood Alam <sawood@archive.org>
2021-11-09 14:40:46 -0500

    Soft wrap table rows

--- cdxsummary/summarizer.py
+++ cdxsummary/summarizer.py
@@ -88,6 +88,10 @@ class ReportSummarizer():
         return sum([bool(sum(v.values())) for v in grid.values()])
 
 
+    def _fit_table_width(self, table):
+        return sum([max([len(cell) for cell in list(col.cells) + [col.header, col.footer]]) for col in table.columns]) + len(table.columns) + 1
+
+
     def _natural_date(self, dt):
         try:
             return naturaldate(datetime.strptime(dt, "%Y%m%d%H%M%S"))
@@ -98,7 +102,7 @@ class ReportSummarizer():
     def __init__(self, report, outfile=sys.stdout):
         self._report = report
         self._replayurl = getenv("REPLAYURL", "https://web.archive.org/web")
-        self._print = Console(file=outfile).print
+        self._print = Console(file=outfile, soft_wrap=True).print
         self._summary = {
             **report,
             "pathquery": self._path_query_grid(report["pathquery"]),
@@ -125,6 +129,7 @@ class ReportSummarizer():
         table.add_row("Total WARC Records Size", naturalsize(self._summary["bytes"]))
         table.add_row("First Memento Date", self._natural_date(self._summary["first"]))
         table.add_row("Last Memento Date", self._natural_date(self._summary["last"]))
+        table.width = self._fit_table_width(table)
         self._print(table)
 
 
@@ -132,14 +137,15 @@ class ReportSummarizer():
         mimestatus = self._summary["mimestatus"]
         manyrows = self._non_zero_grid_rows(mimestatus) > 1
         table = Table(title="MIME Type and Status Code Distribution", box=box.HORIZONTALS, show_header=True, show_footer=manyrows, header_style="bold magenta", footer_style="bold magenta", padding=(0, 0))
-        table.add_column("MIME", "TOTAL", style="bold cyan", overflow="fold")
+        table.add_column("MIME", "TOTAL", style="bold cyan")
         for code in self.CODEGROUPS:
-            table.add_column(code, intcomma(sum([codes[code] for codes in mimestatus.values()])), justify="right", overflow="fold")
-        table.add_column("TOTAL", intcomma(self._summary["captures"]), style="bold cyan", justify="right", overflow="fold")
+            table.add_column(code, intcomma(sum([codes[code] for codes in mimestatus.values()])), justify="right")
+        table.add_column("TOTAL", intcomma(self._summary["captures"]), style="bold cyan", justify="right")
         for mime, codes in mimestatus.items():
             row_total = sum(codes.values())
             if row_total:
                 table.add_row(mime, *map(intcomma, codes.values()), intcomma(row_total))
+        table.width = self._fit_table_width(table)
         self._print(table)
 
 
@@ -147,14 +153,15 @@ class ReportSummarizer():
         pathquery = self._summary["pathquery"]
         manyrows = self._non_zero_grid_rows(pathquery) > 1
         table = Table(title="Path and Query Segments", box=box.HORIZONTALS, show_header=True, show_footer=manyrows, header_style="bold magenta", footer_style="bold magenta", padding=(0, 0))
-        table.add_column("Path", "TOTAL", style="bold cyan", overflow="fold")
+        table.add_column("Path", "TOTAL", style="bold cyan")
         for query in self.QSEGMENTS:
-            table.add_column(query, intcomma(sum([queries[query] for queries in pathquery.values()])), justify="right", overflow="fold")
-        table.add_column("TOTAL", intcomma(self._summary["captures"]), style="bold cyan", justify="right", overflow="fold")
+            table.add_column(query, intcomma(sum([queries[query] for queries in pathquery.values()])), justify="right")
+        table.add_column("TOTAL", intcomma(self._summary["captures"]), style="bold cyan", justify="right")
         for path, queries in pathquery.items():
             row_total = sum(queries.values())
             if row_total:
                 table.add_row(path, *map(intcomma, queries.values()), intcomma(row_total))
+        table.width = self._fit_table_width(table)
         self._print(table)
 
 
@@ -162,14 +169,15 @@ class ReportSummarizer():
         yearmonth = self._summary["yearmonth"]
         manyrows = self._non_zero_grid_rows(yearmonth) > 1
         table = Table(title="Year and Month Distribution", box=box.HORIZONTALS, show_header=True, show_footer=manyrows, header_style="bold magenta", footer_style="bold magenta", padding=(0, 0))
-        table.add_column("Year", "TOTAL", style="bold cyan", overflow="fold")
+        table.add_column("Year", "TOTAL", style="bold cyan")
         for month in self.MONTHS:
-            table.add_column(month, intcomma(sum([months[month] for months in yearmonth.values()])), justify="right", overflow="fold")
-        table.add_column("TOTAL", intcomma(self._summary["captures"]), style="bold cyan", justify="right", overflow="fold")
+            table.add_column(month, intcomma(sum([months[month] for months in yearmonth.values()])), justify="right")
+        table.add_column("TOTAL", intcomma(self._summary["captures"]), style="bold cyan", justify="right")
         for year, months in yearmonth.items():
             row_total = sum(months.values())
             if row_total:
                 table.add_row(year, *map(intcomma, months.values()), intcomma(row_total))
+        table.width = self._fit_table_width(table)
         self._print(table)
 
 
@@ -177,10 +185,11 @@ class ReportSummarizer():
         tophosts = self._summary["tophosts"]
         others = self._summary["hosts"] - len(tophosts)
         table = Table(title=f"Top {len(tophosts)} Out of {intcomma(self._summary['hosts'])} Hosts", box=box.HORIZONTALS, show_header=True, show_footer=(others > 0), header_style="bold magenta", footer_style="bold magenta", padding=(0, 0))
-        table.add_column("Host", f"OTHERS ({intcomma(others)} Hosts)", style="bold cyan", overflow="fold")
-        table.add_column("Captures", intcomma(self._summary["captures"] - sum(tophosts.values())), justify="right", overflow="fold")
+        table.add_column("Host", f"OTHERS ({intcomma(others)} Hosts)", style="bold cyan")
+        table.add_column("Captures", intcomma(self._summary["captures"] - sum(tophosts.values())), justify="right")
         for host, count in tophosts.items():
             table.add_row(host, intcomma(count))
+        table.width = self._fit_table_width(table)
         self._print(table)
 
 
